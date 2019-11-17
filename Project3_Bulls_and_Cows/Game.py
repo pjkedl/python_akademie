395063433c0a9fb69c335dba06039cd6802a0913
Pavel Karol <pavel.karol@gmail.com>
2019-11-17 19:48:12 +0100

    - Bulls and Cows game test with several issues

--- Project3_Bulls_and_Cows/Game.py
+++ Project3_Bulls_and_Cows/Game.py
@@ -3,10 +3,11 @@ import random
 def game_header():
     print('Hi there!')
     print("I've generated a random 4 digit number for you.")
-    number = str(2019)
+    number = str(2017)
     generated_number = [int(x) for x in str(number)]
     # number = str(random.randint(1000,9999))
     print("Let's play a bulls and cows game.")
+    print(generated_number)
     return generated_number
 
 def ask_for_number():
@@ -15,19 +16,47 @@ def ask_for_number():
         try:
             a = int(a)
         except ValueError:
-            print('Please enter 4 digit number only.')
+            print('Please enter number only.')
             continue
         if a not in range(1000, 9999):
-            print('Please enter number in range 1000 - 9999.')
+            print('Please enter 4 digit number in range 1000 - 9999.')
         else:
             guess = [int(x) for x in str(a)]
             break
     return guess
 
-computer_list = game_header()
-my_list =  ask_for_number()
-for index, item in enumerate(my_list):
-    if item in computer_list:
+def bull_cow(counter):
+    computer_list = game_header()
+    my_list = ask_for_number()
+    bull = 0
+    cow = 0
+    for index, item in enumerate(my_list):
+        if item in computer_list:
+            if computer_list[index] == my_list[index]:
+                bull += 1
+            else:
+                cow += 1
+    if bull != 4:
+        print(f'{bull} bulls, {cow} cows')
+    else:
+        print(f"Correct, you've guessed the right number in {counter} guesses!")
+        exit()
+
+def main():
+    counter = 1
+    game_header()
+    while True:
+        ask_for_number()
+        bull_cow(counter)
+        counter += 1
+
+main()
+
+
+
+
+
+
 
 
 
