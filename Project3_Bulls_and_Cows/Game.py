252b163fa097d1211ccd2a7557900ed164aaf79e
Pavel Karol <pavel.karol@gmail.com>
2019-11-30 15:57:28 +0100

    - new version of secret number generator

--- Project3_Bulls_and_Cows/Game.py
+++ Project3_Bulls_and_Cows/Game.py
@@ -8,9 +8,17 @@ def game_header():
 
 
 def number_generator():
-    number = str(random.randint(1000,9999))
-    generated_number = [int(x) for x in str(number)]
-    return generated_number
+    generated_number = [int(x) for x in range(10)]
+    secret = []
+
+    for i in generated_number:
+        number = random.choice(generated_number)
+        if len(secret) == 4:
+            break
+        else:
+            secret.append(number)
+            generated_number.remove(number)
+    return secret
 
 
 def ask_for_number():
